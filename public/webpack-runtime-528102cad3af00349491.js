704e597fa499655267dba01b5cee13ae19f0d9bc
Andrew L Dell'Aringa <166876043+aldellar@users.noreply.github.com>
2025-08-28 12:31:43 -0700

    Update hero text and tech stack, fix trailing comma issue

--- public/webpack-runtime-528102cad3af00349491.js
+++ public/webpack-runtime-528102cad3af00349491.js
@@ -6,27 +6,27 @@
     r,
     o = {},
     c = {};
-  function i(e) {
+  function f(e) {
     var t = c[e];
     if (void 0 !== t) return t.exports;
     var n = (c[e] = { exports: {} });
-    return o[e](n, n.exports, i), n.exports;
+    return o[e](n, n.exports, f), n.exports;
   }
-  (i.m = o),
+  (f.m = o),
     (e = []),
-    (i.O = function (t, n, r, o) {
+    (f.O = function (t, n, r, o) {
       if (!n) {
         var c = 1 / 0;
         for (s = 0; s < e.length; s++) {
           (n = e[s][0]), (r = e[s][1]), (o = e[s][2]);
-          for (var f = !0, u = 0; u < n.length; u++)
+          for (var i = !0, u = 0; u < n.length; u++)
             (!1 & o || c >= o) &&
-            Object.keys(i.O).every(function (e) {
-              return i.O[e](n[u]);
+            Object.keys(f.O).every(function (e) {
+              return f.O[e](n[u]);
             })
               ? n.splice(u--, 1)
-              : ((f = !1), o < c && (c = o));
-          if (f) {
+              : ((i = !1), o < c && (c = o));
+          if (i) {
             e.splice(s--, 1);
             var a = r();
             void 0 !== a && (t = a);
@@ -38,7 +38,7 @@
       for (var s = e.length; s > 0 && e[s - 1][2] > o; s--) e[s] = e[s - 1];
       e[s] = [n, r, o];
     }),
-    (i.n = function (e) {
+    (f.n = function (e) {
       var t =
         e && e.__esModule
           ? function () {
@@ -47,7 +47,7 @@
           : function () {
               return e;
             };
-      return i.d(t, { a: t }), t;
+      return f.d(t, { a: t }), t;
     }),
     (n = Object.getPrototypeOf
       ? function (e) {
@@ -56,18 +56,18 @@
       : function (e) {
           return e.__proto__;
         }),
-    (i.t = function (e, r) {
+    (f.t = function (e, r) {
       if ((1 & r && (e = this(e)), 8 & r)) return e;
       if ('object' == typeof e && e) {
         if (4 & r && e.__esModule) return e;
         if (16 & r && 'function' == typeof e.then) return e;
       }
       var o = Object.create(null);
-      i.r(o);
+      f.r(o);
       var c = {};
       t = t || [null, n({}), n([]), n(n)];
-      for (var f = 2 & r && e; 'object' == typeof f && !~t.indexOf(f); f = n(f))
-        Object.getOwnPropertyNames(f).forEach(function (t) {
+      for (var i = 2 & r && e; 'object' == typeof i && !~t.indexOf(i); i = n(i))
+        Object.getOwnPropertyNames(i).forEach(function (t) {
           c[t] = function () {
             return e[t];
           };
@@ -76,23 +76,23 @@
         (c.default = function () {
           return e;
         }),
-        i.d(o, c),
+        f.d(o, c),
         o
       );
     }),
-    (i.d = function (e, t) {
+    (f.d = function (e, t) {
       for (var n in t)
-        i.o(t, n) && !i.o(e, n) && Object.defineProperty(e, n, { enumerable: !0, get: t[n] });
+        f.o(t, n) && !f.o(e, n) && Object.defineProperty(e, n, { enumerable: !0, get: t[n] });
     }),
-    (i.f = {}),
-    (i.e = function (e) {
+    (f.f = {}),
+    (f.e = function (e) {
       return Promise.all(
-        Object.keys(i.f).reduce(function (t, n) {
-          return i.f[n](e, t), t;
+        Object.keys(f.f).reduce(function (t, n) {
+          return f.f[n](e, t), t;
         }, []),
       );
     }),
-    (i.u = function (e) {
+    (f.u = function (e) {
       return (
         ({
           29: 'component---src-pages-pensieve-index-js',
@@ -121,8 +121,8 @@
         '.js'
       );
     }),
-    (i.miniCssF = function (e) {}),
-    (i.g = (function () {
+    (f.miniCssF = function (e) {}),
+    (f.g = (function () {
       if ('object' == typeof globalThis) return globalThis;
       try {
         return this || new Function('return this')();
@@ -130,14 +130,14 @@
         if ('object' == typeof window) return window;
       }
     })()),
-    (i.o = function (e, t) {
+    (f.o = function (e, t) {
       return Object.prototype.hasOwnProperty.call(e, t);
     }),
     (r = {}),
-    (i.l = function (e, t, n, o) {
+    (f.l = function (e, t, n, o) {
       if (r[e]) r[e].push(t);
       else {
-        var c, f;
+        var c, i;
         if (void 0 !== n)
           for (var u = document.getElementsByTagName('script'), a = 0; a < u.length; a++) {
             var s = u[a];
@@ -147,10 +147,10 @@
             }
           }
         c ||
-          ((f = !0),
+          ((i = !0),
           ((c = document.createElement('script')).charset = 'utf-8'),
           (c.timeout = 120),
-          i.nc && c.setAttribute('nonce', i.nc),
+          f.nc && c.setAttribute('nonce', f.nc),
           c.setAttribute('data-webpack', 'v4:' + n),
           (c.src = e)),
           (r[e] = [t]);
@@ -171,20 +171,20 @@
           l = setTimeout(d.bind(null, void 0, { type: 'timeout', target: c }), 12e4);
         (c.onerror = d.bind(null, c.onerror)),
           (c.onload = d.bind(null, c.onload)),
-          f && document.head.appendChild(c);
+          i && document.head.appendChild(c);
       }
     }),
-    (i.r = function (e) {
+    (f.r = function (e) {
       'undefined' != typeof Symbol &&
         Symbol.toStringTag &&
         Object.defineProperty(e, Symbol.toStringTag, { value: 'Module' }),
         Object.defineProperty(e, '__esModule', { value: !0 });
     }),
-    (i.p = '/'),
+    (f.p = '/'),
     (function () {
       var e = { 311: 0 };
-      (i.f.j = function (t, n) {
-        var r = i.o(e, t) ? e[t] : void 0;
+      (f.f.j = function (t, n) {
+        var r = f.o(e, t) ? e[t] : void 0;
         if (0 !== r)
           if (r) n.push(r[2]);
           else if (311 != t) {
@@ -192,19 +192,19 @@
               r = e[t] = [n, o];
             });
             n.push((r[2] = o));
-            var c = i.p + i.u(t),
-              f = new Error();
-            i.l(
+            var c = f.p + f.u(t),
+              i = new Error();
+            f.l(
               c,
               function (n) {
-                if (i.o(e, t) && (0 !== (r = e[t]) && (e[t] = void 0), r)) {
+                if (f.o(e, t) && (0 !== (r = e[t]) && (e[t] = void 0), r)) {
                   var o = n && ('load' === n.type ? 'missing' : n.type),
                     c = n && n.target && n.target.src;
-                  (f.message = 'Loading chunk ' + t + ' failed.\n(' + o + ': ' + c + ')'),
-                    (f.name = 'ChunkLoadError'),
-                    (f.type = o),
-                    (f.request = c),
-                    r[1](f);
+                  (i.message = 'Loading chunk ' + t + ' failed.\n(' + o + ': ' + c + ')'),
+                    (i.name = 'ChunkLoadError'),
+                    (i.type = o),
+                    (i.request = c),
+                    r[1](i);
                 }
               },
               'chunk-' + t,
@@ -212,14 +212,14 @@
             );
           } else e[t] = 0;
       }),
-        (i.O.j = function (t) {
+        (f.O.j = function (t) {
           return 0 === e[t];
         });
       var t = function (t, n) {
           var r,
             o,
             c = n[0],
-            f = n[1],
+            i = n[1],
             u = n[2],
             a = 0;
           if (
@@ -227,15 +227,15 @@
               return 0 !== e[t];
             })
           ) {
-            for (r in f) i.o(f, r) && (i.m[r] = f[r]);
-            if (u) var s = u(i);
+            for (r in i) f.o(i, r) && (f.m[r] = i[r]);
+            if (u) var s = u(f);
           }
-          for (t && t(n); a < c.length; a++) (o = c[a]), i.o(e, o) && e[o] && e[o][0](), (e[o] = 0);
-          return i.O(s);
+          for (t && t(n); a < c.length; a++) (o = c[a]), f.o(e, o) && e[o] && e[o][0](), (e[o] = 0);
+          return f.O(s);
         },
         n = (self.webpackChunkv4 = self.webpackChunkv4 || []);
       n.forEach(t.bind(null, 0)), (n.push = t.bind(null, n.push.bind(n)));
     })(),
-    (i.nc = void 0);
+    (f.nc = void 0);
 })();
-//# sourceMappingURL=webpack-runtime-d660b526c35d8a04efe6.js.map
+//# sourceMappingURL=webpack-runtime-528102cad3af00349491.js.map
